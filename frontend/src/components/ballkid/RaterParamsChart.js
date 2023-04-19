451e78eb1fb3a59164b17a01a4721b5f7a9ce231
Andrea Iosue <li.andrea4@gmail.com>
2023-04-18 23:12:23 -0400

    clip endpoints for rater chart

--- frontend/src/components/ballkid/RaterParamsChart.js
+++ frontend/src/components/ballkid/RaterParamsChart.js
@@ -9,7 +9,7 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
-import { Line } from "react-chartjs-2";
+import { Scatter } from "react-chartjs-2";
 
 ChartJS.register(
   CategoryScale,
@@ -29,6 +29,8 @@ export function RaterParamsChart({
 }) {
   const options = {
     responsive: true,
+    showLine: true,
+    pointStyle: false,
     plugins: {
       legend: {
         position: "top",
@@ -54,33 +56,41 @@ export function RaterParamsChart({
     },
   };
 
-  const labels = [0.5, 5];
   const data = {
-    labels,
     datasets: [
       {
         label: "Captain",
-        data: [0.5 * scale + offset, 5 * scale + offset],
+        data: [
+          { x: 0.5, y: 0.5 },
+          { x: Math.max((0.5 - offset) / scale, 0.5), y: 0.5 },
+          { x: Math.min((5 - offset) / scale, 5), y: 5 },
+          { x: 5, y: 5 },
+        ],
         borderColor: "rgb(255, 99, 132)",
         backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
       {
         label: "Average",
         data: [
-          0.5 * average_scale + average_offset,
-          5 * average_scale + average_offset,
+          { x: 0.5, y: 0.5 },
+          { x: Math.max((0.5 - average_offset) / average_scale, 0.5), y: 0.5 },
+          { x: Math.min((5 - average_offset) / average_scale, 5), y: 5 },
+          { x: 5, y: 5 },
         ],
         borderColor: "rgb(53, 162, 235)",
         backgroundColor: "rgba(53, 162, 235, 0.5)",
       },
       {
         label: "Ideal",
-        data: [0.5, 5],
+        data: [
+          { x: 0.5, y: 0.5 },
+          { x: 5, y: 5 },
+        ],
         borderColor: "rgb(0, 192, 75)",
         backgroundColor: "rgba(0, 192, 75, 0.5)",
       },
     ],
   };
 
-  return <Line options={options} data={data} />;
+  return <Scatter options={options} data={data} />;
 }
