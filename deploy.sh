6a4b56ddf7f9ceab79ae6eaeb124c727826ce19c
Andrea Iosue <li.andrea4@gmail.com>
2023-06-20 22:42:49 -0400

    add github workflow for autodeploy on release

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+# 1. Build the frontend
+cd frontend
+npm run build
+
+# 2. Move files at the build root inside a root subdirectory
+mkdir -p build/root
+for file in $(ls build | grep -E -v '^(index\.html|static|root)$'); do
+    mv "build/$file" build/root;
+done
+
+cd ..
+
+# 3. Build the backend
+python manage.py collectstatic --no-input
+python manage.py migrate 
