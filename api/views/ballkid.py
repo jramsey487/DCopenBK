1f0a98f2e57dfb26358b0a7a55b7aa9dc9314bce
Andrea Iosue <li.andrea4@gmail.com>
2024-07-18 13:05:27 -0400

    fix bug with avg check in days on ballkid page

--- api/views/ballkid.py
+++ api/views/ballkid.py
@@ -1166,7 +1166,7 @@ class GetAverageCourtLeaderboard(APIView):
 
         averages = annotate_durations(Ballkid.objects.filter(is_active=True)).aggregate(
             checkin_avg=Coalesce(Avg("checkin_duration"), timedelta()),
-            days_avg=Avg("checkinanalytics__count"),
+            days_avg=Coalesce(Avg("checkin_days"), 0),
             court_avg=Coalesce(Avg("court_duration"), timedelta()),
             stadium_avg=Coalesce(Avg("stadium_duration"), timedelta()),
             harris_avg=Coalesce(Avg("harris_duration"), timedelta()),
