132d5d4c95a3e7ac50b78ee6e7d3a294c08051be
Andrea Iosue <li.andrea4@gmail.com>
2024-07-23 18:12:57 -0400

    fix out of town non-rookie not counting as experienced bug

--- api/utils/teams_generator.py
+++ api/utils/teams_generator.py
@@ -32,10 +32,13 @@ class Team:
 
         self.ballkids[position].append(ballkid)
 
+        # A ballkid qualifies as experienced if a captain, chair, supervet (> 3
+        # years experience), or out-of-town non-rookie
         if (
             ballkid.is_captain
             or ballkid.is_chairperson
             or ballkid.num_years_experience > SUPERVET_THRESHOLD
+            or (ballkid.is_out_of_town and ballkid.num_years_experience > 0)
         ):
             self.experienced[position].append(ballkid)
 
@@ -66,17 +69,16 @@ class TeamsGenerator:
                 ]
             ]
 
-    """
-    Returns the smallest team in the list of teams. 
-
-    Arguments: 
-    position(str): If position is not None, then returns the team with the fewest 
-    number of ballkids at that position
-    max_size(int): if max_size is not None, then will not return a team which already 
-    has the max number of ballkids 
-    """
-
     def get_smallest_team(self, position=None, max_size=None):
+        """
+        Returns the smallest team in the list of teams.
+
+        Arguments:
+        position(str): If position is not None, then returns the team with the fewest
+        number of ballkids at that position
+        max_size(int): if max_size is not None, then will not return a team which already
+        has the max number of ballkids
+        """
         # Keep track of both the smallest team and the team with the fewest ballkids at
         # the position of interest
         smallest_team = self.teams[0]
@@ -105,6 +107,28 @@ class TeamsGenerator:
         return eligible_teams[0] if len(eligible_teams) > 0 else None
 
     def create_teams(self):
+        """
+        Creates teams as a list of populated Team objects, satisfying the criteria that:
+        - Each team needs at least one captain or chairperson
+        - Each team at each position needs at least one captain/chairperson/supervet/
+        out-of-town non-rookie
+        - Balance experience
+        - Randomize so the same person doesn't always get the same team / captain
+        - Follows pre-defined order of team strength if relevant (>= 10 teams)
+        - Ideally ballkids are assigned to their preferred position
+
+        General algorithm:
+        - Let us consider 3 disjoint sets of ballkids, fully covering the space of checked
+        in ballkids: captains/chairpeople, supervets (> 3 years experience OR out of town
+        non-rookies), and all else.
+        - First go through captains (randomly ordered) at each position (net and back) and
+        assign them to a team in priority order.
+        - Then go through supervets and assign them to any teams that don't have an experienced
+        ballkid at both positions yet. If all teams have experience at both positions, then
+        assign them to the team with the fewest ballkids at their preferred position.
+        - Finally go through all ballkids. Assign them to the smallest team at their preferred
+        position or if that team is already maxed out, then the smallest team in general
+        """
         all = Ballkid.objects.filter(is_checked_in=True)
         max_ballkids_per_team = math.ceil(len(all) / len(self.teams))
 
@@ -121,9 +145,12 @@ class TeamsGenerator:
         ballkids = list(
             all.exclude(id__in=captains)
             .exclude(id__in=supervets)
+            .order_by("?")
             .order_by("-num_years_experience")
         )
 
+        # Do not restart team_counter to 0 inside for loop to maximize likelihood
+        # that all teams have at least 1 captain
         team_counter = 0
 
         for pos in [POSITION.N, POSITION.B]:
@@ -141,7 +168,9 @@ class TeamsGenerator:
         for supervet in supervets:
             team = self.get_team_without_experienced_position(supervet.position)
             if team is None:
-                team = self.get_smallest_team(supervet.position)
+                team = self.get_smallest_team(
+                    supervet.position, max_size=max_ballkids_per_team
+                )
 
             team.add_ballkid(supervet)
 
